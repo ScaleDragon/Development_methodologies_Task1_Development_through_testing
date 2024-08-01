251bcbc4fc2c31ad77489e488dc9484f7b13a373
Vyacheslav Lukin <dwek18@mail.ru>
2024-08-01 12:10:55 +0300

    Stub and tests for add

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -2,4 +2,10 @@ package ru.netology;
 
 public class PhoneBook {
 
+    public PhoneBook() {
+    }
+
+    public int  add(String name, int number) {
+        return 0;
+    }
 }
--- src/test/java/netology/PhoneBookTest.java
+++ src/test/java/netology/PhoneBookTest.java
@@ -1,5 +1,23 @@
 package netology;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import ru.netology.PhoneBook;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class PhoneBookTest {
-    
+    private Set<String> contactList = new HashSet<>();
+
+    @ParameterizedTest
+    @CsvSource({"Маша, 9034568723" ," Паша, 9052431478 "," Анна,9088764572","Маша,9035673957"})
+    public void add(String name, int number) {
+        final int countContact = 3;
+        PhoneBook phoneBook = new PhoneBook();
+       int result = phoneBook.add(name,number);
+    Assertions.assertEquals(countContact, result);
+    }
+
 }
